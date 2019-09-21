36854c0ac534b4571d13260f32b93ad12e8ebd3d
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-21 18:22:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each(){ |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each(){ |holiday, supplys| 
-      puts "  #{holiday.to_s.split() }: #{supplies}"
+      puts "  #{holiday.to_s.split("_") }: #{supplies}"
     }
   }
 end
