f9c5add44b01bee16cb5d6c432805d4593e22825
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-21 18:21:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each(){ |season, holiday|
+  holiday_hash.each(){ |season, holidays|
     puts "#{season.capitalize}:"
-    holiday.each(){
+    holidays.each(){ |holiday, supply|
   }
 end
 
